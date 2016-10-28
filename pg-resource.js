9c49e2609ecc197e53a9a7b0262f4154553d0f02
Akash Srivastava <vyomsrivastav@gmail.com>
2016-10-28 12:51:40 +0530

    Initial Publish

---
+++ pg-resource.js
@@ -0,0 +1,82 @@
+//created by Akash
+(function(){
+	'use strict';
+	angular.module('pg-resource',[])
+	.service('ApiConfig',[function(){
+		this.baseUrl = "";
+	}])
+	.factory('Resource',['$http','$q',function($http,$q){
+		return{
+			create: function(url){
+				//break the url in array at every /(backlash)
+				var finalUrlC = url.trim();
+				var resturl = (url.trim()).split("/");
+				var urlValue = [];
+				for(partialUrl in resturl){
+					if(resturl[partialUrl].search(":")!=-1){
+						urlValue.push(resturl[partialUrl].replace(":",""));
+					}
+				}
+				
+				//function to map the data to url
+				function buildUrl(data){
+					var finalUrl = finalUrlC;
+					for(k in urlValue){
+						if(data[urlValue[k]]){
+							var d = (":"+urlValue[k]).trim();
+							finalUrl= finalUrl.replace(d,data[urlValue[k]]);
+						}
+					}
+					finalUrl= finalUrl.replace(":id","");
+					return finalUrl;
+					
+				}
+				
+				//function to map the qdata and urldata to url with query to be used in get methods
+				function buildqUrl(urlData,qData){
+					var qUrl = buildUrl(urlData);
+					
+					var keys = qData.keys;
+					keys= keys.trim();
+					
+					keys = keys.split(":");
+					var qString = "?";
+					for(x in keys){
+						if(x==0){qString = qString+ keys[x]+"=" + qData[keys[x]];}
+						else{qString = qString + "&" + keys[x] +"="+ qData[keys[x]];}
+					}
+					//console.log(qUrl+qString);
+					return qUrl+qString;
+				}
+				
+				
+			return {
+				//Defining http service for all
+				all: function(urlData){
+					return $http.get(buildUrl(urlData));
+				},
+				//This will return the data of a given id
+				find: function(urlData){
+					return $http.get(buildUrl(urlData));
+				},
+				//This will process the data with query string
+				qfind: function(urlData,qData){
+					return $http.get(buildqUrl(urlData,qData));
+				},
+				//This will update the data of resource with given id
+				update: function(urlData,data){
+					return $http.patch(buildUrl(urlData),data);
+				},
+				//This will create the classroom
+				create: function(urlData,data){
+					return $http.post(buildUrl(urlData),data);
+				},
+				//To delete the data
+				destroy: function(urlData){
+					return $http.delete(buildUrl(urlData));
+				}
+			};
+		}
+		};
+	}]);
+});
